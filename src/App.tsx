e1c041c8103f60192c6328d1e13269b58acad85e
cdawg1213 <103866953+cdawg1213@users.noreply.github.com>
2024-03-08 09:08:11 -0700

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const myData = require('./CollegeBasketballTeams.json')
+const teams = myData["teams"]
+
+interface props {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function App() {  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <List/>
     </div>
   );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <h1 className="header-title">Welcome to NCAA College Basketball</h1>
+        <p className="header-description">Below you will find information on all the colleges in NCAA Basketball.</p>
+      </div>
+    </header>
+  );
+}
+
+function List() {
+  return (
+    <div className="card-container">
+      { teams.map((blah: props) => (<School {...blah} />)) }
+    </div>
+  )
+}
+
+class School extends React.Component<props>
+{
+  render() {
+    const college = this.props;
+
+    return (
+      <div className="card">
+      <div className="card-content">
+        <h2 className="card-title">{college.school}</h2>
+        <h3 className="card-description">{college.name}</h3>
+          <p>{college.city}, {college.state}</p>
+      </div>
+    </div>
+    )
+  }
 }
 
 export default App;
